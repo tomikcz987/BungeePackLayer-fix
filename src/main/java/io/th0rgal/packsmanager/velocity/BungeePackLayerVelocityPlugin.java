cd422c1c32c2ecca217f0c03add06a814b333008
Adam Volkman (maku) <itismaku@gmail.com>
2024-09-15 17:58:15 +0200

    fix(velocity): invalid ResourcePackRequest class, removed protocolize

--- src/main/java/io/th0rgal/packsmanager/velocity/BungeePackLayerVelocityPlugin.java
+++ src/main/java/io/th0rgal/packsmanager/velocity/BungeePackLayerVelocityPlugin.java
@@ -1,18 +1,12 @@
 package io.th0rgal.packsmanager.velocity;
 
 import com.google.inject.Inject;
-
+import com.velocitypowered.api.event.EventManager;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
-import com.velocitypowered.api.plugin.Dependency;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.proxy.ProxyServer;
-
-import dev.simplix.protocolize.api.Protocolize;
-
-import io.th0rgal.packsmanager.velocity.listeners.DisconnectListener;
 import io.th0rgal.packsmanager.velocity.listeners.ResourcePackRequestListener;
-
 import org.slf4j.Logger;
 
 @Plugin(
@@ -20,45 +14,21 @@ import org.slf4j.Logger;
         name = "Bungee Pack Layer",
         version = "${version}",
         description = "${description}",
-        authors = {"th0rgal"},
-        dependencies = {
-                @Dependency(id = "protocolize")
-        }
+        authors = {"th0rgal", "itismaku"}
 )
 public class BungeePackLayerVelocityPlugin {
 
     private final ProxyServer proxyServer;
-    private final Logger logger;
-    private static BungeePackLayerVelocityPlugin INSTANCE;
-    private ResourcePackRequestListener resourcePackRequestListener;
 
     @Inject
     public BungeePackLayerVelocityPlugin(ProxyServer proxyServer, Logger logger) {
         this.proxyServer = proxyServer;
-        this.logger = logger;
     }
 
     @Subscribe
     public void onProxyInitialization(ProxyInitializeEvent event) {
-        INSTANCE = this;
-        this.resourcePackRequestListener = new ResourcePackRequestListener();
-        Protocolize.listenerProvider().registerListener(resourcePackRequestListener);
-        this.proxyServer.getEventManager().register(this, new DisconnectListener());
+        EventManager eventManager = proxyServer.getEventManager();
+        eventManager.register(this, new ResourcePackRequestListener());
     }
 
-    public ProxyServer getProxyServer() {
-        return this.proxyServer;
-    }
-
-    public Logger getLogger() {
-        return this.logger;
-    }
-
-    public static BungeePackLayerVelocityPlugin getINSTANCE() {
-        return INSTANCE;
-    }
-
-    public ResourcePackRequestListener getResourcePackRequestListener() {
-        return this.resourcePackRequestListener;
-    }
 }
--- src/main/java/io/th0rgal/packsmanager/velocity/listeners/ResourcePackRequestListener.java
+++ src/main/java/io/th0rgal/packsmanager/velocity/listeners/ResourcePackRequestListener.java
@@ -1,41 +1,37 @@
 package io.th0rgal.packsmanager.velocity.listeners;
 
-import com.velocitypowered.proxy.protocol.packet.ResourcePackRequest;
+import com.google.common.collect.Maps;
+import com.velocitypowered.api.event.PostOrder;
+import com.velocitypowered.api.event.ResultedEvent;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.connection.DisconnectEvent;
+import com.velocitypowered.api.event.player.ServerResourcePackSendEvent;
+import com.velocitypowered.api.proxy.player.ResourcePackInfo;
 
-import dev.simplix.protocolize.api.Direction;
-import dev.simplix.protocolize.api.listener.AbstractPacketListener;
-import dev.simplix.protocolize.api.listener.PacketReceiveEvent;
-import dev.simplix.protocolize.api.listener.PacketSendEvent;
-
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.UUID;
 
-public class ResourcePackRequestListener extends AbstractPacketListener<ResourcePackRequest> {
+public class ResourcePackRequestListener {
 
-    private final Map<UUID, String> map = new HashMap<>();
+    private final Map<UUID, byte[]> map = Maps.newHashMap();
 
-    public ResourcePackRequestListener() {
-        super(ResourcePackRequest.class, Direction.DOWNSTREAM, 0);
-    }
-
-    @Override
-    public void packetReceive(PacketReceiveEvent<ResourcePackRequest> event) {
-        final ResourcePackRequest resourcePackRequest = event.packet();
-        final UUID uuid = event.player().uniqueId();
-        if (map.containsKey(uuid) && map.get(uuid).equals(resourcePackRequest.getHash())) {
-            event.cancelled(true);
+    @Subscribe(order = PostOrder.FIRST)
+    public void packetReceive(ServerResourcePackSendEvent event) {
+        ResourcePackInfo resourcePackInfo = event.getProvidedResourcePack();
+        UUID uuid = event.getServerConnection().getPlayer().getUniqueId();
+        byte[] packHash = resourcePackInfo.getHash();
+        if (map.containsKey(uuid) && Arrays.equals(map.get(uuid), packHash)) {
+            event.setResult(ResultedEvent.GenericResult.denied());
             return;
         }
-        map.put(uuid, resourcePackRequest.getHash());
+
+        map.put(uuid, packHash);
     }
 
-    @Override
-    public void packetSend(PacketSendEvent<ResourcePackRequest> event) {
-
+    @Subscribe(order = PostOrder.FIRST)
+    public void onDisconnectEvent(DisconnectEvent event) {
+        map.remove(event.getPlayer().getUniqueId());
     }
 
-    public void removePlayer(final UUID uuid) {
-        map.remove(uuid);
-    }
 }
